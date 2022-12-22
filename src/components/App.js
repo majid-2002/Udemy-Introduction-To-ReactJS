56f4629fbdfe8a73f6d54551c822c242d3d8297e
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2022-12-22 10:33:58 +0530

    created the inline sytles

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react"
-import { ReactDOM } from "react-dom"
-import List from "./List"
-import Heading from "./Heading"
+import React from "react";
+import { ReactDOM } from "react-dom";
+import List from "./List";
+import Heading from "./Heading";
 
+let name = "Majid";
+const customStyle = {
+  color: "red",
+  fontSize: "50px"
+};
 
-function App(){
-    return (
-        <div>
-            <Heading></Heading>
-            <List></List>
-            <List></List>
-        </div>
-    )
+let newColor = (customStyle.color = "blue");
+
+function App() {
+  return (
+    <div>
+      <h1 style={customStyle}>{name}</h1>
+      <Heading></Heading>
+      <List></List>
+      <List></List>
+    </div>
+  );
 }
-
 
 export default App;
