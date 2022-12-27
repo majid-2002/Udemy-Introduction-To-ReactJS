96446619329f21ef259ab25aeb62e67c491a16cd
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2022-12-27 10:57:51 +0530

    Destructure and UseState in React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,37 @@
 /* eslint-disable */
 import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = true;
 
 function App() {
+  const [count, setcount] = React.useState(0);
+
+  // function increase(){
+  //   setcount(count+1);
+  // }
+  // function decrease(){
+  //   setcount(count-1);
+  // }
+
+//** const [red , green, blue] = [250 , 60 , 7 ]  ----- this is destructuring in react , where we can acces the array element by a name */
+ 
+
+
   return (
     <div className="container">
-      <Form isregistered={userIsRegistered} />
+
+      <h1>{count}</h1>
+
+      <button
+        onClick={() => {
+          setcount(count + 1);
+        }}
+      >+</button>
+
+      <button
+        onClick={() => {
+          setcount(count - 1);
+        }}
+      >-</button>
+
     </div>
   );
 }
