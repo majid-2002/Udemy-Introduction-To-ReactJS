c65cbf5a8cfe23b79605753b56b95346c0a5ab36
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2022-12-28 23:25:49 +0530

    Learnt about destructuring in detail

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
+import cars from "./practice";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//? Array destructuring
+const [tesla, honda] = cars;
 
-//Challenge:
-//1. Given that you can get the current time using:
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
+//? Object destructuring
+const {
+    speedStats: {
+        topspeed: teslaTopSpeed 
+    }
+} = tesla;
 
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
+//? Object destructuring with custome name
+const {
+    speedStats: {
+        topspeed: hondaTopSpeed 
+    }
+} = honda;
 
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
+const {
+    colourByPopularity: [teslaTopColour]
+} = tesla
+
+const {
+    colourByPopularity: [hondaTopColour]
+} = honda
+
+
+
+ReactDOM.render(
+    <table>
+        <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+            <th>Top Colour</th>
+        </tr>
+        <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+        </tr>
+
+    </table>
+    
+    , document.getElementById("root"));
